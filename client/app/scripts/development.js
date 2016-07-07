9a6183acff0c9378929cce50d6e5b311f605ebfc
= <Eric.Irwin1124@gmail.com>
2016-07-06 20:35:14 -0700

    Fixed release proxy

--- client/app/scripts/development.js
+++
@@ -1,2 +0,0 @@
-angular.module("yapp")
-.constant("ENV", {"name":"development","apiEndpoint":"http://localhost:8181/api/v1"});
---
+++ users/requests.go
@@ -0,0 +1,26 @@
+package users
+
+
+type createUserRequest struct {
+
+}
+
+type createUserResponse struct  {
+
+}
+
+type getUserRequest struct {
+
+}
+
+type getUserResponse struct {
+
+}
+//
+//func (r *createUserRequest) Validate() (bool,[]string) {
+//
+//}
+//
+//func (r *getUserRequest) Validate() (bool,[]string)  {
+//
+//}
