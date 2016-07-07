1cc48115b7586a5779814a4eb783308bb27dce19
= <Eric.Irwin1124@gmail.com>
2016-07-07 12:54:26 -0700

    Flipped logic for cache token

--- polls/services.go
+++ polls/services.go
@@ -123,7 +123,7 @@ func (s *service) CreateResponse(itemId, pollId int, token string) (models.Respo
 
 	var response models.Response
 
-	if err != nil || val == "0" {
+	if err != nil || val == "1" {
 		return response, err
 	}
 
